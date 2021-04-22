8f5ff4076cbd0c2d5f06f90ca8bee54e25c0a132
Fillipi Pereira <wenanzer@gmx.de>
2021-04-22 05:23:41 +0200

    More development

---
+++ src/routes.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { getCustomRepository } from "typeorm";
+
+import { SettingsRepository } from "./repositories/SettingRepositories";
+
+const routes = Router();
+
+/***
+* Tipos de parâmetros
+* Routes Params => Parâmetros de rotas
+* Query Params => Filtros e buscases
+* http://localhost:333/settings/1?search=algumacoisa
+*
+* Body params => {
+*
+* }
+*/
+
+routes.post("/settings", async (request, response) => {
+  const { chat, username } = request.body;
+
+  const settingsRepository = getCustomRepository(SettingsRepository)
+
+  const settings = settingsRepository.create({
+    chat,
+    username
+  })
+
+  await settingsRepository.save(settings);
+
+  return response.json(settings);
+})
+
+export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,12 @@
 import express from "express";
 
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
-/***
- * GET = Buscas 
- * POST = Criação 
- * PUT = Alteração
- * DELETA = Deletar
- * PATCH = Alterar uma informação específica
- */
+app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Hello Next Level Week 05!",
-  });
-});
-
-app.post("/", (request, response) => {
-  return response.json({ message: "User successfully saved!" });
-})
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
