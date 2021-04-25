65b9b8af979453a7aee496bd5c342b42ce102d30
Fillipi Pereira <wenanzer@gmx.de>
2021-04-25 07:40:17 +0200

    Backu

--- src/routes.ts
+++ src/routes.ts
@@ -1,34 +1,10 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { SettingsController } from "./controllers/SettingsControllers";
 
 const routes = Router();
 
-/***
-* Tipos de parâmetros
-* Routes Params => Parâmetros de rotas
-* Query Params => Filtros e buscases
-* http://localhost:333/settings/1?search=algumacoisa
-*
-* Body params => {
-*
-* }
-*/
+const settingsController = new SettingsController();
 
-routes.post("/settings", async (request, response) => {
-  const { chat, username } = request.body;
-
-  const settingsRepository = getCustomRepository(SettingsRepository)
-
-  const settings = settingsRepository.create({
-    chat,
-    username
-  })
-
-  await settingsRepository.save(settings);
-
-  return response.json(settings);
-})
+routes.post("/settings", settingsController.create);
 
 export { routes };
