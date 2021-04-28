4382733919528b4d02775237a400710ffefd4426
Fillipi Pereira <wenanzer@gmx.de>
2021-04-28 15:20:56 +0200

    Interface Websocket

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,11 +1,11 @@
 const socket = io();
-
+let connectionsUsers = [];
 
 socket.on("admin_list_all_users", (connections) => {
+  connectionsUsers = connections;
+  document.getElementById("list_users").innerHTML = "";
 
-  document.getElementByID("list_users").innerHTML = "";
-
-  let template = document.getElementByID("template").innerHTML;
+  let template = document.getElementById("template").innerHTML;
 
   connections.forEach(connection => {
 
@@ -16,4 +16,54 @@ socket.on("admin_list_all_users", (connections) => {
 
     document.getElementById("list_users").innerHTML += rendered
   })
-})
+});
+
+function call(id) {
+
+  const connection = connectionsUsers.find(connection => connection.socket_id === id);
+
+  const template = document.getElementById("admin_template").innerHTML;
+
+  const rendered = Mustache.render(template, {
+    email: connection.user.email,
+    id: connection.user_id
+  })
+
+  document.getElementById("supports").innerHTML += rendered;
+
+  const params = {
+    user_id: connection.user_id
+  }
+
+  socket.emit("admin_list_messages_by_user", params, messages => {
+
+    const divMessages = document.getElementById(
+      `allMessages${connection.user_id}`
+    );
+
+    messages.forEach(message => {
+      const createDiv = document.createElement("div");
+
+      if (message.admin_id === null) {
+        createDiv.className = "admin_message_client";
+
+        createDiv.innerHTML = `<span>${connection.user.email} - ${message.text}</span>`;
+        createDiv.innerHTML += `<span class+"admin_date> ${dayjs(
+          message.created_at
+        ).format("DD/MM/YYYY HH:mm:ss")}`
+
+      } else {
+        createDiv.className = "admin_message_admin";
+
+        createDiv.innerHTML = `Atendente: <span>${message.text}</span>`;
+        createDiv.innerHTML += `<span class+"admin_date> ${dayjs(
+          message.created_at
+        ).format("DD/MM/YYYY HH:mm:ss")}`
+      }
+
+
+      divMessages.appendChild(createDiv);
+    })
+
+  });
+}
